bda40360e43202fa563ee87e22cd3faa09ffb422
Javad <bc_alimohammadi@yahoo.com>
2018-01-13 22:40:51 +0330

    first commit!

---
+++ src/main/java/server/Main.java
@@ -0,0 +1,18 @@
+package server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: javad
+ * Date: 4/6/13
+ * Time: 11:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+}
