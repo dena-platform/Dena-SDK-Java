26112a18566895df3d8985a49de417215083901b
Javad <bc_alimohammadi@yahoo.com>
2018-01-13 22:45:53 +0330

    first commit!

---
+++ src/main/java/com/dena/client/Main.java
@@ -0,0 +1,12 @@
+package com.dena.client;
+
+/**
+ * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+}
