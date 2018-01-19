39c2f981578a6d0a545b149ecf5af71463904406
Javad <bc_alimohammadi@yahoo.com>
2018-01-19 23:48:34 +0330

    first commit!

--- src/main/java/com/dena/client/reflectionUtils/ReflectionUtils.java
+++ src/main/java/com/dena/client/reflectionUtils/ReflectionUtils.java
@@ -1,8 +1,31 @@
 package com.dena.client.reflectionUtils;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
  */
+
 public final class ReflectionUtils {
-    
+    /**
+     * Find all public fields in class or inherited class (not interface)
+     *
+     * @param object
+     * @return
+     */
+    public static List<Field> findAllFields(Object object) {
+        Class klass = object.getClass();
+        List<Field> fieldList = new LinkedList<>();
+
+        while (klass != null) {
+            Field[] fields = klass.getDeclaredFields();
+            fieldList.addAll(Arrays.asList(fields));
+            klass = klass.getSuperclass();
+        }
+        return fieldList;
+    }
+
 }
